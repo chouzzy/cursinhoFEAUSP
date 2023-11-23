cfc32a42a26437c8594b39fdf658c8dccacab0a3
mathf <Mathias>
2023-11-23 16:19:46 -0300

    teste sem body validation

--- src/modules/registrations/useCases/SchoolClass/updateSchoolClass/UpdateSchoolClassUseCase.ts
+++ src/modules/registrations/useCases/SchoolClass/updateSchoolClass/UpdateSchoolClassUseCase.ts
@@ -11,21 +11,21 @@ class UpdateSchoolClassUseCase {
     async execute(schoolClassData: UpdateSchoolClassRequestProps, schoolClassID: SchoolClass["id"]): Promise<validationResponse> {
 
         // Validate the body sent from the frontend service
-        const bodyValidation = await checkBody(schoolClassData)
+        // const bodyValidation = await checkBody(schoolClassData)
 
-        return {
-            isValid: true,
-            successMessage: "paramos aqui",
-            statusCode: 203
-        }
+        // return {
+        //     isValid: true,
+        //     successMessage: "paramos aqui",
+        //     statusCode: 203
+        // }
 
-        if (bodyValidation.isValid === false) {
-            return ({
-                isValid: false,
-                statusCode: 403,
-                errorMessage: bodyValidation.errorMessage,
-            })
-        }
+        // if (bodyValidation.isValid === false) {
+        //     return ({
+        //         isValid: false,
+        //         statusCode: 403,
+        //         errorMessage: bodyValidation.errorMessage,
+        //     })
+        // }
 
         schoolClassData.selectiveStages.map( stage => {
             if (!stage.resultsDate) {
