096911383910bcc6763e9677f04a7364ad9aef42
mathf <Mathias>
2023-11-24 14:54:50 -0300

    2411v3

--- src/modules/registrations/useCases/SchoolClass/updateSchoolClass/UpdateSchoolClassUseCase.ts
+++ src/modules/registrations/useCases/SchoolClass/updateSchoolClass/UpdateSchoolClassUseCase.ts
@@ -11,16 +11,18 @@ class UpdateSchoolClassUseCase {
     async execute(schoolClassData: UpdateSchoolClassRequestProps, schoolClassID: SchoolClass["id"]): Promise<validationResponse> {
 
         // Validate the body sent from the frontend service
-        const bodyValidation = await checkBody(schoolClassData)
+        console.log('schoolClassData')
+        console.log(schoolClassData)
 
+        // const bodyValidation = await checkBody(schoolClassData)
 
-        if (bodyValidation.isValid === false) {
-            return ({
-                isValid: false,
-                statusCode: 403,
-                errorMessage: bodyValidation.errorMessage,
-            })
-        }
+        // if (bodyValidation.isValid === false) {
+        //     return ({
+        //         isValid: false,
+        //         statusCode: 403,
+        //         errorMessage: bodyValidation.errorMessage,
+        //     })
+        // }
 
         schoolClassData.selectiveStages.map( stage => {
             if (!stage.resultsDate) {
