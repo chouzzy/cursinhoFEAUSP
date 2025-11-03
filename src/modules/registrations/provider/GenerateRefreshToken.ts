f8e4428b5f60a88a07e0c385fdc60d3f040978aa
chouzzy <mathfernando@hotmail.com>
2025-11-03 16:03:21 -0300

    upsert

--- src/modules/registrations/provider/GenerateRefreshToken.ts
+++ src/modules/registrations/provider/GenerateRefreshToken.ts
@@ -5,22 +5,29 @@ import { RefreshToken } from "../entities/RefreshToken";
 
 class GenerateRefreshToken {
 
-    async execute(adminID: RefreshToken["adminID"]): Promise<RefreshToken> {
+    async execute(adminID: RefreshToken["adminID"]): Promise<RefreshToken> {
 
-        const expTimeNumber = Number(process.env.REFRESHTOKEN_EXPIRATION_TIME_NUMBER)
-        // const expTimeUnit = process.env.REFRESHTOKEN_EXPIRATION_TIME_UNIT
+        const expTimeNumber = Number(process.env.REFRESHTOKEN_EXPIRATION_TIME_NUMBER)
+        // const expTimeUnit = process.env.REFRESHTOKEN_EXPIRATION_TIME_UNIT
 
-        const expires_at = dayjs().add(expTimeNumber, 'days').unix();
+        const expires_at = dayjs().add(expTimeNumber, 'days').unix();
 
-        const generateRefreshToken = await prisma.refreshToken.create({
-            data: {
-                adminID,
-                expires_at
-            }
-        })
+        // **CORREÇÃO AQUI:** Trocamos 'create' por 'upsert'
+        const newRefreshToken = await prisma.refreshToken.upsert({
+            where: {
+                adminID: adminID, // Onde procurar (o campo único)
+            },
+            update: {
+                expires_at: expires_at, // O que fazer se ele já existir (só atualiza a data de expiração)
+            },
+            create: {
+                adminID: adminID, // O que fazer se ele não existir
+                expires_at: expires_at
+            }
+        })
 
-        return generateRefreshToken
-    }
+        return newRefreshToken
+    }
 }
 
 export { GenerateRefreshToken }
